6c3fe0cb6e98b803664ee0ecf0e471e307840c89
Franziskeer <fpa19@alu.ua.es>
2019-12-09 18:12:48 +0100

    Instalada librería de permisos y roles y seeders de muestra

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('role_has_permissions')->truncate();
+        DB::table('model_has_permissions')->truncate();
+        DB::table('model_has_roles')->truncate();
+
+        DB::table('users')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('permissions')->truncate();      
+
+        $this->call([
+            PermissionsTableSeeder::class,
+            UsersTableSeeder::class,
+        ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
