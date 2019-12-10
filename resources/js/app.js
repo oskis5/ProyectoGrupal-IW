f917f4f95f47499834b567574d7bb71ba492a6f9
Óscar Luis Pérez Pina <oskis5@hotmail.com>
2019-12-10 16:42:52 +0100

    vue instalado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Vue from 'vue';
+import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+const app = new Vue({
+    el: '#app',
+    render: h => h(App),
+});
+
+
+export default app;
---
+++ resources/views/app.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
