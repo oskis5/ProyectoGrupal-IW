b452bfe95643e11415f9a279ca999444b06268d0
Franziskeer <fpa19@alu.ua.es>
2019-12-13 13:15:51 +0100

    Prototipo de barra de navegación

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" :src="'images/logo.png'">
+    <b-navbar toggleable="lg" type="light" variant="light" class="py-0">
+      <a class="navbar-brand my-0" href="#">
+        <img alt="Vue logo" :src="'images/logo-h.png'" class="img-fluid" style="height: 70px">
+      </a>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>Nombre de usuario</em>
+          </template>
+          <b-dropdown-item href="#">Perfil</b-dropdown-item>
+          <b-dropdown-item href="#">Cerrar sesión</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -23,6 +39,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue'
 import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
 Vue.use(Vuex);
+
 const app = new Vue({
     el: '#app',
     render: h => h(App),
