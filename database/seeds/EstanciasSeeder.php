f325da3c5fe5918620fa1630f4ed7d7f38f5b706
Jordi Ferrer <jferrerbeltran@gmail.com>
2019-12-13 18:53:14 +0100

    Seeders de las diferentes tablas del proyecto

---
+++ database/seeds/EstanciasSeeder.php
@@ -0,0 +1,120 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EstanciasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *   Schema::create('estancias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('tipo_id');
+            $table->foreign('tipo_id')->references('id')->on('tipo_habitaciones');
+            $table->integer('planta');
+            $table->integer('puerta');
+            $table->string('nombre');
+            $table->mediumText('descripcion');
+            $table->boolean('reservada');
+            $table->float('precio_base');
+
+            //Habitaciones
+            $table->float('metros_cuadrados');
+            $table->integer('camas');
+
+            //Sala reuniones
+            $table->integer('capacidad');
+            
+            $table->timestamps();
+        });
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('estancias')->insert([
+            [
+                'tipo_id' => 1,
+                'planta' => 1,
+                'puerta' => 1,
+                'reservada' => false,
+                'metros_cuadrados' => 20,
+                'camas' => 1,
+                'nombre' => 'Habitación 101',
+                'descripcion' => '',
+                'precio_base' => 0,
+                'capacidad' => 0
+            ],
+            [
+                'tipo_id' => 1,
+                'planta' => 2,
+                'puerta' => 1,
+                'reservada' => false,
+                'metros_cuadrados' => 22,
+                'camas' => 1,
+                'nombre' => 'Habitación 201',
+                'descripcion' => '',
+                'precio_base' => 0,
+                'capacidad' => 0
+            ],
+            [
+                'tipo_id' => 2,
+                'planta' => 1,
+                'puerta' => 2,
+                'reservada' => false,
+                'metros_cuadrados' => 25,
+                'camas' => 1,
+                'nombre' => 'Habitación 202',
+                'descripcion' => '',
+                'precio_base' => 0,
+                'capacidad' => 0
+            ],
+            [
+                'tipo_id' => 1,
+                'planta' => 2,
+                'puerta' => 3,
+                'reservada' => false,
+                'metros_cuadrados' => 20,
+                'camas' => 1,
+                'nombre' => 'Habitación 202',
+                'descripcion' => 'Esta habitación tiene vistas al mar.',
+                'precio_base' => 0,
+                'capacidad' => 0
+            ],
+            [
+                'tipo_id' => 3,
+                'planta' => 3,
+                'puerta' => 1,
+                'reservada' => false,
+                'metros_cuadrados' => 90,
+                'camas' => 2,
+                'nombre' => 'Suite Jordi Ferrer',
+                'descripcion' => 'Esta sala está especialmente equipada para el amor.',
+                'precio_base' => 0,
+                'capacidad' => 0
+            ],
+            [
+                'tipo_id' => 4,
+                'planta' => 0,
+                'puerta' => 1,
+                'reservada' => false,
+                'metros_cuadrados' => 150,
+                'camas' => 0,
+                'nombre' => 'Sala Vanesa Lara',
+                'descripcion' => 'Gran sala de reuniones con butacas y proyector.',
+                'precio_base' => 0,
+                'capacidad' => 100 
+            ],
+            [
+                'tipo_id' => 4,
+                'planta' => 0,
+                'puerta' => 2,
+                'reservada' => false,
+                'metros_cuadrados' => 20,
+                'camas' => 0,
+                'nombre' => 'Sala Fran Piñero',
+                'descripcion' => 'Sala para pequeñas reuniones informales con mesa, sillones y ordenador.',
+                'precio_base' => 0,
+                'capacidad' => 20 
+            ]
+        ]);
+    }
+}
--- database/seeds/ReservaTableSeeder.php
+++ database/seeds/ReservaTableSeeder.php
@@ -1,16 +1,50 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class ReservaTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     *  Schema::create('reservas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('estancia_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('tipo_id');
+            $table->unsignedBigInteger('temporada_id');
+            $table->date('f_entrada');
+            $table->date('f_salida');
+            $table->foreign('estancia_id')->references('id')->on('estancias');
+            $table->foreign('cliente_id')->references('id')->on('users');
+            $table->float('precio_total');
+            $table->foreign('tipo_id')->references('id')->on('tipo_reservas');
+            $table->foreign('temporada_id')->references('id')->on('temporadas');
+            $table->timestamps();
+        });
      * @return void
      */
     public function run()
     {
-        //
+        DB::table('reservas')->insert([
+            [
+                'estancia_id' => 1,
+                'cliente_id' => 1,
+                'tipo_id' => 1,
+                'temporada_id' => 3,
+                'f_entrada' => Carbon::createFromDate(2019, 12, 12),
+                'f_salida' => Carbon::createFromDate(2019, 12, 15),
+                'precio_total' => 120
+            ],
+            [
+                'estancia_id' => 5,
+                'cliente_id' => 1,
+                'tipo_id' => 1,
+                'temporada_id' => 3,
+                'f_entrada' => Carbon::createFromDate(2019, 2, 14),
+                'f_salida' => Carbon::createFromDate(2019, 2, 15),
+                'precio_total' => 420
+            ],
+        ]);   
     }
 }
