2dc79e95097ca38abc7097b565af7a91eff74163
Franziskeer <fpa19@alu.ua.es>
2020-01-16 21:14:52 +0100

    Añadido formato de fechas

--- resources/js/components/ItemReserva.vue
+++ resources/js/components/ItemReserva.vue
@@ -20,19 +20,22 @@ export default {
     },
     created: async function(){
         this.getDatos()
-        var f_entrada = this.estancia.f_entrada
-        console.log(f_entrada)
     },
     methods: {
         getDatos: async function(){
             var estancia = await fetch('http://localhost/ProyectoGrupal-IW/public/api/estancias/'+this.reserva.estancia_id)
             this.estancia = await estancia.json()
-
+            
             var tipo = await fetch('http://localhost/ProyectoGrupal-IW/public/api/tipoestancias/'+this.reserva.tipo_id)
             this.tipo = await tipo.json()
 
             var temporada = await fetch('http://localhost/ProyectoGrupal-IW/public/api/temporadas/'+this.reserva.temporada_id)
             this.temporada = await temporada.json()
+
+            var f_entrada = new Date(this.reserva.f_entrada)
+            this.reserva.f_entrada = f_entrada.toLocaleDateString("es-ES")
+            var f_salida = new Date(this.reserva.f_salida)
+            this.reserva.f_salida = f_salida.toLocaleDateString("es-ES")
         },
     }
 }
